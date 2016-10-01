6257c8cb46156ed55d2bf6ba2e6b7cae778aae68
Cody Reandeau <codyreandeau@yahoo.com>
2016-10-01 10:21:03 -0400

    Created the insert method.

--- BSTintergers/BST.java
+++ BSTintergers/BST.java
@@ -2,4 +2,27 @@ public class BST {
  
   public Node root;
   
+/**
+ * Insert method with one parameter
+ */
+  void insert(int data) {
+    insert(root, data);
+  }
+  
+/**
+ * Method to insert a new node in order
+ * in our binary search tree
+ */
+  Node insert(Node node, int data){
+    if(root==null){
+      node = new Node(data);
+      return node;
+    }
+    if(data < root.getData()){
+      node.setLeft(insert(node.getLeft(), data));
+    }else if (data > root.getData()){
+      node.setRight(insert(node.getRight(), data));
+  }
+    return node;
+  }
 }
--- BSTintergers/Main.java
+++ BSTintergers/Main.java
@@ -2,5 +2,14 @@ public class Main {
   
   public static void main(String[] args) {
     
+    BST tree = new BST();
+    
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(5);
+    
   }
 }
